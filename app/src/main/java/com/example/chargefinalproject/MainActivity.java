1cad10db351042d2b7a5fdc4d3ed8ee342ade885
mobinaheydari <heydari.mobina8227@gmail.com>
2023-06-15 14:22:41 +0330

    add intents to activity

--- app/src/main/java/com/example/chargefinalproject/MainActivity.java
+++ app/src/main/java/com/example/chargefinalproject/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.chargefinalproject;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    Button fixedLineButton;
+    Button chargeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chargeButton = (Button) findViewById(R.id.button_charge);
+        fixedLineButton = (Button) findViewById(R.id.button_fixedline);
+
+        chargeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ChargeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        fixedLineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FixedLineActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
